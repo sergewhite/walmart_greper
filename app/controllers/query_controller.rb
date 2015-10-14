0368da72a9d1385561bef17efddd8d821ebe9011
Serge Majstruk <majstruk@gera-it.com>
2015-10-14 16:09:08 +0300

    [^] typo fix

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -2,8 +2,6 @@ class QueryController < ApplicationController
   layout "main"
 
   def create
-    result = Greper.new(options).get_reviews
-    binding.pry
-    1+1
+    result = Greper.new(params[:query]).get_reviews
   end
 end
